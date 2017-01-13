4758372102c5d125de86092c851fd24dbb426991
NATHAN FORTNER <271114@district.pylusd.k12.ca.us>
2017-01-13 07:48:09 -0800

    update

---
+++ test.java
@@ -0,0 +1,11 @@
+import java.util.Map;
+
+public class test
+{
+    // instance variables - replace the example below with your own
+    public void word0(String[] strings) {
+        for(String i:strings)
+            System.out.println(i);
+    }
+
+}
